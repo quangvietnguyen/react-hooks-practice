7d4f649b13abaf99b9f8dca732f34efc6980e7bd
nguye374 <nguye374@sheridancollege.ca>
2021-10-19 03:07:08 -0400

    Oct 19th 2nd

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ ingredients, setIngredients ] = useState([]);
+
+  useEffect();
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://practice-hooks-91596-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type' : 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredients(prevIngredients => [
+        ...prevIngredients, 
+        { id: responseData.name, ...ingredient }])
+    });
+  }
+
+  const removeItemHandler = (id) => {
+    setIngredients(prevIngredients => prevIngredients.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={removeItemHandler}/>
       </section>
     </div>
   );
