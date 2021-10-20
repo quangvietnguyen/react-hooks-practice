294e4630dc87aac36ac7d5c7b1e916e26c5cf1c7
nguye374 <nguye374@sheridancollege.ca>
2021-10-19 21:31:10 -0400

    Oct 19 3rd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth'
 
 const App = props => {
-  return <Ingredients />;
+  // return <Ingredients />;
+  return <Auth/>
 };
 
 export default App;
